4011d2baede6984f99014d2d6d3ef5d311af9d06
Carlos Eduardo Rodríguez Flores <cerf02062007@gmail.com>
2026-09-08 16:07:29 -0600

    Clases
    Agregacion de clase categoria y producto

--- src/main/java/ni/edu/uam/fact_app/models/Categoria.java
+++ src/main/java/ni/edu/uam/fact_app/models/Categoria.java
@@ -1,4 +1,20 @@
 package ni.edu.uam.fact_app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Categoria {
+    private String id;
+    private String nombre;
+    private  boolean activa;
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
 }
--- src/main/java/ni/edu/uam/fact_app/models/Producto.java
+++ src/main/java/ni/edu/uam/fact_app/models/Producto.java
@@ -1,4 +1,22 @@
 package ni.edu.uam.fact_app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
+    private Integer id;
+    private String codigo;
+    private String nombre;
+    private Categoria categoria;
+    private BigDecimal precioVenta;
+    private int existencia;
+    private String rutaImagen;
+    private  boolean activo;
 }
