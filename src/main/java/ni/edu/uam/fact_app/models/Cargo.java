c7cd4d20e46733247d68c9d8b81edcbab864db7c
Carlos Eduardo Rodríguez Flores <cerf02062007@gmail.com>
2026-09-08 16:21:45 -0600

    update Cargo Empleado

--- src/main/java/ni/edu/uam/fact_app/models/Cargo.java
+++ src/main/java/ni/edu/uam/fact_app/models/Cargo.java
@@ -1,4 +1,16 @@
 package ni.edu.uam.fact_app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.ref.PhantomReference;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private  String descripcion;
 }
--- src/main/java/ni/edu/uam/fact_app/models/Empleado.java
+++ src/main/java/ni/edu/uam/fact_app/models/Empleado.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.fact_app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Empleado {
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private String cargo;
+    private LocalDate fechaContracion;
+    private  boolean activo;
 }
